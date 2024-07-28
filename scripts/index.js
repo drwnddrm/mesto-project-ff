d3c4871eb9945a4539207e421aa8ad20471c19f1
Artem Pokatilov <dr.artpokatilov@yandex.ru>
2024-07-28 09:12:20 +0300

    Added buttons and cards in places

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+const addButton = content.querySelector('.profile__add-button');
+
+let index = 0;
+
+const addPlace = (title, link, callback) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = title;
+  cardElement.querySelector('.card__image').alt = title;
+  cardElement.querySelector('.card__image').src = link;
+  
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) {
+    callback(cardElement);
+  });
+
+  placesList.append(cardElement);
+}
+
+const removePlace = (cardElement) => {
+  cardElement.remove();
+}
+
+addButton.addEventListener('click', () => {  
+  let titleName = initialCards[index].name;
+  let imageLink = initialCards[index].link;
+  
+  addPlace(titleName, imageLink, removePlace);
+  index++;
+});
